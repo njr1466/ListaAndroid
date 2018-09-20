f2b23cbc7a12c979a7bcd8a840fa338e2fd7eefa
NIlson <njr1466@gmail.comm>
2018-09-20 13:08:43 -0300

    segundo commit

--- app/src/main/java/com/example/nilsonjunior/lista/MainActivity.java
+++ app/src/main/java/com/example/nilsonjunior/lista/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Boolean variavel = true;
 
         lista = (ListView) findViewById(R.id.listaId);
 
